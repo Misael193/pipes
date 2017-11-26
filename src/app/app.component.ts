35c0b646fb10796b3f1045efd55a98e2cad8fdb6
Misael193 <misael.62886@hotmail.com>
2017-11-25 21:43:25 -0600

    agrega mas pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre = "Misael";
+  nombre2 = "miSael arMAndo piNtor gONzález";
   arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI = Math.PI;
+  a = 0.234;
+  salario = 1234.5;
+  heroe = {
+    nombre:"Logan",
+    clave:"Wolverine",
+    edad:500,
+    direccion:{
+      calle:"Siempre viva",
+      casa:"123"
+    }
+  }
+
+  valorDePromesa = new Promise((resolve, reject)=>{
+    setTimeout(()=>resolve('¡Llegó la data!'),3500);
+  });
+
+  fecha = new Date();
+
+  video = "29PIDSCWmQE";
+
+  activar:boolean = true;
 }
---
+++ src/app/pipes/contrasena.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'contrasena'
+})
+export class ContrasenaPipe implements PipeTransform {
+
+  transform(value: string, activar: boolean): string {
+    if(activar){
+      value = value.replace(/./g,"*");
+    }
+    return value;
+  }
+
+}
